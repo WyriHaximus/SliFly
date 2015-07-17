20c092787659c708a31c243e7f67e7a19707ac66
Cees-Jan Kiewiet <ceesjank@gmail.com>
2015-07-17 07:42:08 +0200

    White line

--- tests/FlysystemServiceProviderTest.php
+++ tests/FlysystemServiceProviderTest.php
@@ -13,7 +13,6 @@ class FlysystemServiceProviderTest extends \PHPUnit_Framework_TestCase
             'registerFlysystems',
         ]);
 
-
         $provider
             ->expects($this->once())
             ->method('registerFlysystems')
